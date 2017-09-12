a41653bcde52e2335bdc7a92d0c7afd955024994
qube <bquenin@axway.com>
2017-09-11 19:46:46 -0700

    Iterative trie

--- search/trie.go
+++ search/trie.go
@@ -1,34 +1,116 @@
 package search
 
-import "github.com/bquenin/algorithms/ds"
+import (
+	"github.com/bquenin/algorithms/ds"
+)
+
+type Trie interface {
+	Contains(key string) bool
+	Get(key string) interface{}
+	Put(key string, value interface{})
+	CountKeysWithPrefix(prefix string) int
+	Keys() ds.Queue
+	KeysWithPrefix(prefix string) ds.Queue
+}
 
 type trieNode struct {
-	next  map[uint8]*trieNode
+	next  map[byte]*trieNode
 	value interface{}
 	size  int
 }
 
-func NewTrieNode() *trieNode { return &trieNode{next: map[uint8]*trieNode{}} }
+func NewTrieNode() *trieNode { return &trieNode{next: map[byte]*trieNode{}} }
 
-type Trie struct {
+type IterativeTrie struct {
 	root *trieNode
 }
 
-func NewTrie() *Trie { return &Trie{} }
+func NewIterativeTrie() *IterativeTrie { return &IterativeTrie{root: NewTrieNode()} }
 
-func (t *Trie) Contains(key string) bool {
+func (t *IterativeTrie) Contains(key string) bool {
 	return t.Get(key) != nil
 }
 
-func (t *Trie) Get(key string) interface{} {
-	x := t.get(t.root, key, 0)
-	if x == nil {
-		return nil
+func (t *IterativeTrie) get(key string) *trieNode {
+	node, exists := t.root, false
+	for i := 0; i < len(key); i++ {
+		if node, exists = node.next[key[i]]; !exists {
+			return nil
+		}
 	}
-	return x.value
+	return node
 }
 
-func (t *Trie) get(node *trieNode, key string, index int) *trieNode {
+func (t *IterativeTrie) Get(key string) interface{} {
+	node := t.get(key)
+	if node == nil {
+		return nil
+	}
+	return node.value
+}
+
+func (t *IterativeTrie) Put(key string, value interface{}) {
+	node, exists := t.root, false
+	for i := 0; i < len(key); i++ {
+		if _, exists = node.next[key[i]]; !exists {
+			node.next[key[i]] = NewTrieNode()
+		}
+		node = node.next[key[i]]
+		node.size++
+	}
+	node.value = value
+}
+
+func (t *IterativeTrie) CountKeysWithPrefix(prefix string) int {
+	node := t.get(prefix)
+	if node == nil {
+		return 0
+	}
+	return node.size
+}
+
+func (t *IterativeTrie) Keys() ds.Queue {
+	return t.KeysWithPrefix("")
+}
+
+func (t *IterativeTrie) KeysWithPrefix(prefix string) ds.Queue {
+	results := ds.NewArrayQueue()
+	node := t.get(prefix)
+	t.collect(node, prefix, results)
+	return results
+}
+
+func (t *IterativeTrie) collect(node *trieNode, prefix string, results ds.Queue) {
+	if node == nil {
+		return
+	}
+	if node.value != nil {
+		results.Enqueue(prefix)
+	}
+	for k, v := range node.next {
+		t.collect(v, prefix+string(k), results)
+	}
+}
+
+type RecursiveTrie struct {
+	root *trieNode
+}
+
+func NewRecursiveTrie() *RecursiveTrie { return &RecursiveTrie{} }
+
+func (t *RecursiveTrie) Contains(key string) bool {
+	return t.Get(key) != nil
+}
+
+func (t *RecursiveTrie) Get(key string) interface{} {
+	node := t.get(t.root, key, 0)
+	if node == nil {
+		return nil
+	}
+	return node.value
+}
+
+func (t *RecursiveTrie) get(node *trieNode, key string, index int) *trieNode {
 	if node == nil {
 		return nil
 	}
@@ -39,11 +121,11 @@ func (t *Trie) get(node *trieNode, key string, index int) *trieNode {
 	return t.get(node.next[c], key, index+1)
 }
 
-func (t *Trie) Put(key string, value interface{}) {
+func (t *RecursiveTrie) Put(key string, value interface{}) {
 	t.root = t.put(t.root, key, value, 0)
 }
 
-func (t *Trie) put(node *trieNode, key string, value interface{}, index int) *trieNode {
+func (t *RecursiveTrie) put(node *trieNode, key string, value interface{}, index int) *trieNode {
 	if node == nil {
 		node = NewTrieNode()
 	}
@@ -57,7 +139,7 @@ func (t *Trie) put(node *trieNode, key string, value interface{}, index int) *tr
 	return node
 }
 
-func (t *Trie) CountKeysWithPrefix(prefix string) int {
+func (t *RecursiveTrie) CountKeysWithPrefix(prefix string) int {
 	node := t.get(t.root, prefix, 0)
 	if node == nil {
 		return 0
@@ -65,18 +147,18 @@ func (t *Trie) CountKeysWithPrefix(prefix string) int {
 	return node.size
 }
 
-func (t *Trie) Keys() ds.Queue {
+func (t *RecursiveTrie) Keys() ds.Queue {
 	return t.KeysWithPrefix("")
 }
 
-func (t *Trie) KeysWithPrefix(prefix string) ds.Queue {
+func (t *RecursiveTrie) KeysWithPrefix(prefix string) ds.Queue {
 	results := ds.NewArrayQueue()
 	node := t.get(t.root, prefix, 0)
 	t.collect(node, prefix, results)
 	return results
 }
 
-func (t *Trie) collect(node *trieNode, prefix string, results ds.Queue) {
+func (t *RecursiveTrie) collect(node *trieNode, prefix string, results ds.Queue) {
 	if node == nil {
 		return
 	}
--- search/trie_test.go
+++ search/trie_test.go
@@ -6,9 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTrie(t *testing.T) {
+func TestRecursiveTrie(t *testing.T) {
 	input := []string{"she", "sells", "sea", "shells", "by", "the", "sea", "shore"}
-	trie := NewTrie()
+	trie := NewRecursiveTrie()
+	for i, word := range input {
+		trie.Put(word, i)
+	}
+	for _, word := range input {
+		assert.True(t, trie.Contains(word))
+	}
+	assert.Len(t, trie.KeysWithPrefix("sh").ToArray(), 3)
+	assert.Equal(t, trie.CountKeysWithPrefix("sh"), 3)
+	assert.Len(t, trie.Keys().ToArray(), len(input)-1)
+}
+
+func TestIterativeTrie(t *testing.T) {
+	input := []string{"she", "sells", "sea", "shells", "by", "the", "sea", "shore"}
+	trie := NewIterativeTrie()
 	for i, word := range input {
 		trie.Put(word, i)
 	}
