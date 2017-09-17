173d22218e3130e8a7b417a2d1fb09d33ab333bb
qube <bquenin@axway.com>
2017-09-16 19:30:08 -0700

    strings search

--- search/suffixarray.go
+++ search/suffixarray.go
@@ -23,4 +23,9 @@ func NewSuffixArray(s string) SuffixArray {
 	return sa
 }
 
-func (sa *SuffixArray) LCP(i int) int { return len(LCP((*sa)[i-1].Text, (*sa)[i].Text)) }
+func (sa *SuffixArray) LCP(i int) int {
+	if i <= 0 || i >= len(*sa) {
+		return -1
+	}
+	return len(LCP((*sa)[i-1].Text, (*sa)[i].Text))
+}
